b1c88d8d7580f42eca0fca3792907a36861b3925
EhsanAshoori <ehsanehsan2761@gmail.com>
2024-12-28 15:54:04 +0330

    make the layout responsive

--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@ import './assets/scss/main.scss'
 import './index.css'
 
 import { createApp } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+library.add(faUser);
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app')
--- src/views/ExamPage.vue
+++ src/views/ExamPage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ref, reactive, computed, onMounted} from "vue"
+import {useInterval} from '@vueuse/core'
 
 const questions = reactive([
   {
@@ -129,6 +130,8 @@ const questionCounter = ref(1)
 const currentAnswerId = ref(null)
 const savedAnswers = reactive([])
 const indicator = ref()
+const mobileIndicator = ref()
+const timer = ref(1800)
 
 function goToNextQuestion() {
   if (currentQuestionId.value >= questions.length) return
@@ -192,28 +195,36 @@ function finishExam() {
 
 function setAnswerOnIndicator() {
   const index = questionCounter.value - 1
-  const boxes = document.querySelectorAll('.indicator__box')
+  const boxes = indicator.value.querySelectorAll('.indicator__box')
+  const mobileBoxes = mobileIndicator.value.querySelectorAll('.indicator__box')
+  boxes[index].classList.remove('indicator__box--current')
+  mobileBoxes[index].classList.remove('indicator__box--current')
   if (currentAnswerId.value) {
-    boxes[index].classList.remove('indicator__box--current')
-
     boxes[index].classList.add('indicator__box--answered')
+    mobileBoxes[index].classList.add('indicator__box--answered')
     boxes[index].classList.remove('indicator__box--not-answered')
+    mobileBoxes[index].classList.remove('indicator__box--not-answered')
   } else {
-    boxes[index].classList.remove('indicator__box--current')
-
     boxes[index].classList.remove('indicator__box--answered')
+    mobileBoxes[index].classList.remove('indicator__box--answered')
     boxes[index].classList.add('indicator__box--not-answered')
+    mobileBoxes[index].classList.add('indicator__box--not-answered')
   }
 }
 
 function setCurrentClassOnIndicatorBox() {
-  document.querySelectorAll('.indicator__box')[questionCounter.value - 1].classList.add('indicator__box--current')
+  indicator.value.querySelectorAll('.indicator__box')[questionCounter.value - 1].classList.add('indicator__box--current')
+  mobileIndicator.value.querySelectorAll('.indicator__box')[questionCounter.value - 1].classList.add('indicator__box--current')
 }
 
-function jumpToQuestion(index) {
-  // Check if the jump is not logical
-  const el = document.querySelectorAll('.indicator__box')[index]
-  if (!el.classList.contains('indicator__box--answered') && !el.classList.contains('indicator__box--not-answered')) return
+function jumpToQuestion(index, container) {
+  let clickedEl
+  if (container === 'mobile') {
+    clickedEl = mobileIndicator.value.querySelectorAll('.indicator__box')[index]
+  } else {
+    clickedEl = indicator.value.querySelectorAll('.indicator__box')[index]
+  }
+  if (!clickedEl.classList.contains('indicator__box--answered') && !clickedEl.classList.contains('indicator__box--not-answered')) return
 
   saveAnswer()
   setAnswerOnIndicator()
@@ -226,8 +237,8 @@ function jumpToQuestion(index) {
 }
 
 function scrollToNextIndicator() {
-  const nextIndicator = Array.from(document.querySelectorAll('.indicator__box'))[questionCounter.value - 1]
-  const nextIndicatorRect = nextIndicator.getBoundingClientRect()
+  const nextIndicatorRect =
+      Array.from(indicator.value.querySelectorAll('.indicator__box'))[questionCounter.value - 1].getBoundingClientRect()
   const indicatorRect = indicator.value.getBoundingClientRect()
 
   if (nextIndicatorRect.bottom > indicatorRect.bottom) {
@@ -236,11 +247,23 @@ function scrollToNextIndicator() {
       behavior: 'smooth'
     })
   }
+
+  // For mobile
+  const mobileNextIndicatorRect =
+      Array.from(mobileIndicator.value.querySelectorAll('.indicator__box'))[questionCounter.value - 1].getBoundingClientRect()
+  const mobileIndicatorRect = mobileIndicator.value.getBoundingClientRect()
+
+  if (mobileNextIndicatorRect.right > mobileIndicatorRect.right) {
+    mobileIndicator.value.scrollTo({
+      left: mobileIndicator.value.scrollLeft + (mobileNextIndicatorRect.width + 30),
+      behavior: 'smooth'
+    })
+  }
 }
 
 function scrollToPreviousIndicator() {
-  const prevIndicator = Array.from(document.querySelectorAll('.indicator__box'))[questionCounter.value - 1]
-  const prevIndicatorRect = prevIndicator.getBoundingClientRect()
+  const prevIndicatorRect =
+      Array.from(indicator.value.querySelectorAll('.indicator__box'))[questionCounter.value - 1].getBoundingClientRect()
   const indicatorRect = indicator.value.getBoundingClientRect()
 
   if (prevIndicatorRect.top < indicatorRect.top) {
@@ -249,8 +272,38 @@ function scrollToPreviousIndicator() {
       behavior: 'smooth'
     })
   }
+
+  // For mobile
+  const mobilePrevIndicatorRect =
+      Array.from(mobileIndicator.value.querySelectorAll('.indicator__box'))[questionCounter.value - 1].getBoundingClientRect()
+  const mobileIndicatorRect = mobileIndicator.value.getBoundingClientRect()
+
+  if (mobilePrevIndicatorRect.left < mobileIndicatorRect.left) {
+    mobileIndicator.value.scrollTo({
+      left: mobileIndicator.value.scrollLeft - (mobilePrevIndicatorRect.width + 30),
+      behavior: 'smooth'
+    })
+  }
 }
 
+function handleTimer() {
+  const {pause} = useInterval(1000, {
+    callback() {
+      timer.value--
+      if (timer.value === 0) {
+        // Close the exam if timer has ended
+        pause()
+      }
+    },
+    controls: true
+  })
+}
+
+const timerLabel = computed(() => {
+  const minute = Math.trunc(timer.value / 60)
+  const second = timer.value % 60
+  return `${String(minute).padStart(2, '0')}:${String(second).padStart(2, '0')}`
+})
 
 const currentQuestion = computed(() =>
     questions.find(q => q.id === currentQuestionId.value)
@@ -258,6 +311,8 @@ const currentQuestion = computed(() =>
 
 onMounted(async () => {
   document.querySelector('.indicator__box').classList.add('indicator__box--current')
+  mobileIndicator.value.querySelector('.indicator__box').classList.add('indicator__box--current')
+  handleTimer()
 })
 
 </script>
@@ -300,16 +355,34 @@ onMounted(async () => {
         </div>
         <div class="header__item">
           <img src="../assets/images/clock_icon.svg" alt="header cap" class="header__icon">
-          <span class="header__text">زمان باقی مانده آزمون: ۲۳:۱۰</span>
+          <span class="header__text">زمان باقی مانده آزمون: {{ timerLabel }}</span>
         </div>
       </header>
+      <div class="mobile-user-info">
+        <div class="mobile-user-info__box">
+          <div class="sidebar__little-box">نام و نام خانوادگی</div>
+          <div class="sidebar__big-box">محمد رحمانی</div>
+        </div>
+        <div class="mobile-user-info__box">
+          <div class="sidebar__little-box">کد کلاسی</div>
+          <div class="sidebar__big-box">۵۷۶</div>
+        </div>
+        <div class="mobile-user-info__box">
+          <div class="sidebar__little-box">کد دانشجویی</div>
+          <div class="sidebar__big-box">۱۲۳۴۵۶۷۸۹۰</div>
+        </div>
+      </div>
       <div class="questions">
         <div class="question">
           <div class="question__buttons">
-            <button class="next-button" v-if="questionCounter < questions.length" @click="goToNextQuestion"><- سوال
-              بعد
+            <button class="next-button" v-if="questionCounter < questions.length" @click="goToNextQuestion">
+              <i class="fa-solid fa-caret-right"></i>
+              <span>سوال بعد</span>
             </button>
-            <button class="prev-button" v-if="questionCounter > 1" @click="goToPreviousQuestion"> سوال قبل -></button>
+            <button class="prev-button" v-if="questionCounter > 1" @click="goToPreviousQuestion">
+              <span>سوال قبل</span>
+              <i class="fa-solid fa-caret-left"></i>
+            </button>
           </div>
           <h3 class="question__question">سوال {{ questionCounter }}</h3>
           <span
@@ -331,7 +404,16 @@ onMounted(async () => {
           </button>
         </div>
         <div class="indicator" ref="indicator">
-          <div v-for="(q, i) in questions" class="indicator__box" @click="jumpToQuestion(i)">{{ i + 1 }}</div>
+          <div v-for="(q, i) in questions" class="indicator__box" @click="jumpToQuestion(i, 'desktop')">{{
+              i + 1
+            }}
+          </div>
+        </div>
+      </div>
+      <div class="mobile-indicator" ref="mobileIndicator">
+        <div v-for="(q, i) in questions" class="indicator__box"
+             @click="jumpToQuestion(i, 'mobile')"
+        >{{ i + 1 }}
         </div>
       </div>
       <div class="footer">
@@ -350,314 +432,5 @@ onMounted(async () => {
 </template>
 
 <style scoped lang="scss">
-.page {
-  padding: 1.5rem;
-  min-height: 100vh;
-  background-color: var(--page-background-color);
-  display: flex;
-  gap: 1.5rem;
-  align-items: stretch;
-  font-size: 1.2rem;
-  color: var(--text-color);
-}
-
-.sidebar {
-  width: 18%;
-  padding: 2rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  background: var(--box-background-color) url(../assets/images/sidebar_background.jpg) no-repeat;
-  background-position: 50% 10%;
-  background-size: cover;
-  border: 3px solid var(--box-border-color);
-  border-radius: 1.5rem;
-
-  @media only screen and (max-width: 1200px) {
-    display: none;
-  }
-
-  &__logo {
-    width: 8rem;
-  }
-
-  &__title {
-    font-size: 1.6rem;
-    color: var(--color-primary);
-    margin-top: 2rem;
-    font-weight: bold;
-  }
-
-  &__user {
-    margin-top: 1.5rem;
-    border-radius: 50%;
-    padding: .2rem;
-    border: 2px dashed var(--color-primary);
-  }
-
-  &__user-image {
-    width: 7rem;
-    height: 7rem;
-  }
-
-  &__little-box {
-    font-size: .8rem;
-    background-color: var(--color-primary);
-    color: white;
-    padding: .3rem .7rem .1rem;
-    border-radius: .75rem .75rem 0 0;
-    margin-top: 1rem;
-  }
-
-  &__big-box {
-    border-radius: .75rem;
-    padding: .6rem;
-    background-color: var(--color-secondary);
-    width: 100%;
-    text-align: center;
-    font-size: 1.2rem;
-  }
-}
-
-.main {
-  width: 82%;
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-  @media only screen and (max-width: 1200px) {
-    width: 100%;
-  }
-}
-
-.line {
-  border-top: 3px solid var(--box-border-color);
-  width: 100%;
-}
-
-.user-info {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: -3rem;
-}
-
-.header {
-  padding: .5rem;
-  color: white;
-  background-color: var(--color-primary);
-  border-radius: 1rem;
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-  justify-content: center;
-  flex-wrap: wrap;
-
-  &__item {
-    display: flex;
-    align-items: center;
-  }
-
-  &__icon {
-    margin-left: .5rem;
-    width: 2rem;
-    //Hex code of this color : #B7E1FF
-    filter: invert(86%) sepia(12%) saturate(3000%) hue-rotate(180deg) brightness(100%) contrast(100%);
-  }
-}
-
-.questions {
-  display: flex;
-  gap: 1.5rem;
-  user-select: none;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-}
-
-.footer {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-}
-
-.guide, .number-of-questions {
-  background-color: var(--color-gray-1);
-  padding: .5rem 2rem;
-  border-radius: 1rem;
-}
-
-.guide {
-  display: flex;
-  align-items: center;
-
-  div {
-    display: inline-block;
-  }
-
-  span:not(:last-child) {
-    margin-left: 1rem;
-  }
-
-  &__box {
-    width: 1.75rem;
-    height: 1.75rem;
-    margin-left: .5rem;
-    border-radius: .5rem;
-
-    &--success {
-      background-color: var(--color-success);
-    }
-
-    &--danger {
-      background-color: var(--color-danger);
-    }
-
-    &--active {
-      background-color: var(--color-primary);
-    }
-  }
-}
-
-.indicator {
-  border: 3px solid var(--box-border-color);
-  background-color: var(--box-background-color);
-  border-radius: 1rem;
-  padding: .5rem .5rem;
-  max-height: 37rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  overflow: auto;
-  scrollbar-width: none;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  &__box {
-    width: 3rem;
-    height: 3rem;
-    background-color: var(--color-secondary);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 1.75rem;
-    border-radius: .5rem;
-    color: var(--color-primary);
-    cursor: pointer;
-    transition: all .2s ease;
-
-    &--not-answered {
-      background-color: var(--color-danger);
-      color: #75514F;
-    }
-
-    &--answered {
-      background-color: var(--color-success);
-      color: #2D584A;
-    }
-
-    &--current {
-      background-color: var(--color-primary);
-      color: white;
-    }
-
-    &:not(:last-child) {
-      margin-bottom: .75rem;
-    }
-  }
-}
-
-.next-button, .prev-button {
-  padding: .5rem .75rem;
-  background-color: var(--color-secondary);
-  border-radius: .5rem;
-  font-size: 1rem;
-}
-
-.prev-button {
-  background-color: var(--color-gray-1);
-}
-
-.question {
-  border: 3px solid var(--box-border-color);
-  background-color: var(--box-background-color);
-  border-radius: 1.5rem;
-  padding: 1.5rem;
-  flex-grow: 1;
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-
-  &__buttons {
-    display: flex;
-    align-self: flex-end;
-    gap: 1rem;
-  }
-
-  &__question {
-    font-weight: bold;
-    color: var(--color-primary);
-    font-size: 1.5rem;
-  }
-
-  &__answer {
-    display: flex;
-    align-items: center;
-    padding: .5rem .5rem .5rem 4rem;
-    border-radius: .5rem;
-    background-color: var(--color-secondary);
-    cursor: pointer;
-    transition: all .2s ease-out;
-
-    &--selected {
-      background-color: var(--color-primary);
-      color: white;
-    }
-
-    &:not(:last-child) {
-      margin-bottom: 1rem;
-    }
-
-    &__number {
-      width: 2.5rem;
-      height: 2.5rem;
-      display: flex;
-      border-radius: .5rem;
-      justify-content: center;
-      align-items: center;
-      background-color: white;
-      color: var(--color-primary);
-      margin-left: 1rem;
-    }
-
-    &__text {
-      font-size: 1.1rem;
-    }
-  }
-}
-
-.finish-button {
-  padding: .75rem 1rem;
-  background-color: var(--color-gray-2);
-  display: flex;
-  margin-top: 2rem;
-  border-radius: 1rem;
-  font-size: 1rem;
-  align-items: center;
-  cursor: default;
-
-  &--open {
-    background-color: var(--color-success);
-    cursor: pointer;
-  }
-
-  &__icon {
-    margin-left: .5rem;
-    width: 1.5rem;
-  }
-}
-
+@import "@/assets/scss/exam";
 </style>
